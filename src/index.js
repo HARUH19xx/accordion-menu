dcbb4652261925f935fb5a5e1b2f57cbbb334f05
H.Hokari <hookhull@gmail.com>
2022-01-11 01:46:57 +0900

    fix explanation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import './style.scss'
 
 const parentMenu = document.querySelectorAll('.menu li a')
-const parentMenus = document.querySelector('.menu li a')
 
 for (let i =0; i <parentMenu.length; i++) {
     const anchor = parentMenu[i].nextElementSibling
     parentMenu[i].addEventListener('click', (e) => {
         e.preventDefault()
-        parentMenus.classList.remove('active')
         parentMenu[i].classList.toggle('active')
         anchor.classList.toggle('active')
     })
